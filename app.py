ff7d7c9a2c3247f513b934f27ead3562fbcaf8b0
jumsheedk <93278208+jumsheedk@users.noreply.github.com>
2021-10-28 09:48:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from waitress import serve
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+def index():
+    title = 'EDC Data Lineage'
+    return render_template("index.html", title=title)
+
+
+@app.route('/about')
+
+def about():
+    names = ['John','Frank','Mary','Elsa']
+    return render_template("about.html", names=names)
